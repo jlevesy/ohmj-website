744024d8698688b4dfd27d60d47fb2e26d8112a4
Julien Levesy <jlevesy@gmail.com>
2015-11-22 18:46:21 -0800

    setup clean, attempted to setup livereload but failed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
   bower = require('gulp-bower'),
-  less = require('gulp-less');
+  less = require('gulp-less'),
+  livereload = require('gulp-livereload'),
+  http = require('http'),
+  st = require('st'),
+  del = require('del');
 
 
 logError = function(error) {
@@ -19,11 +23,32 @@ gulp.task('less', function() {
                 ]
              }))
              .on('error', logError)
-             .pipe(gulp.dest('dist/css'));
+             .pipe(gulp.dest('dist/css'))
+             .pipe(livereload());
 });
 
 gulp.task('html', function() {
-  return gulp.src('src/index.html').pipe(gulp.dest('dist'));
+  return gulp.src('src/index.html')
+             .pipe(gulp.dest('dist'))
+             .pipe(livereload());
 });
 
 gulp.task('build', ['html', 'less']);
+
+gulp.task('watch', ['build'], function() {
+  livereload.listen({ basePath: 'dist'});
+  gulp.watch('src/less/**/*.less', ['less']);
+  gulp.watch('src/*.html', ['html']);
+});
+
+gulp.task('serve', ['watch'], function(done) {
+  http.createServer(
+    st({ path: __dirname + '/dist', index: 'index.html', cache: false })
+  ).listen(8080, done);
+});
+
+gulp.task('clean', function() {
+  return del(['dist/*'])
+});
+
+gulp.task('default', ['clean', 'build'])
