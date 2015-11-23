fa6595682127c1ca42c3ec3f2a6645d43e4fb8f4
Julien Levesy <jlevesy@gmail.com>
2015-11-22 18:09:18 -0800

    setup less pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  bower = require('gulp-bower'),
+  less = require('gulp-less');
+
+
+logError = function(error) {
+  console.log(error)
+}
+
+gulp.task('bower', function() {
+  return bower().pipe(gulp.dest('bower_components'));
+});
+
+gulp.task('less', function() {
+  return gulp.src('src/less/app.less')
+             .pipe(less({
+                paths: [
+                  __dirname + '/bower_components/bootstrap/less',
+                ]
+             }))
+             .on('error', logError)
+             .pipe(gulp.dest('dist/css'));
+});
