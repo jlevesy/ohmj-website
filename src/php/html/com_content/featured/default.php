f3109cd9b04bd4f8411297c2d174a16ad082d071
Julien Levesy <jlevesy@gmail.com>
2016-01-02 15:52:10 +0100

    cleanup old implementation

--- src/php/html/com_content/featured/default.php
+++ src/php/html/com_content/featured/default.php
@@ -5,58 +5,10 @@ JHtml::_('behavior.caption');
 
 $cfg = JFactory::getConfig();
 ?>
-<?php if ($this->params->get('show_page_heading') != 0) : ?>
-<?php endif; ?>
-<?php if (!empty($this->lead_items)) : ?>
-  <div id="main-carousel"
-       class="carousel big-carousel slide"
-       data-interval="10000"
-       data-pause="hover"
-       data-ride="carousel">
-    <ol class="carousel-indicators">
-      <?php $indic_idx = 0; ?>
-      <?php foreach ($this->lead_items as &$item) : ?>
-        <li data-target="#main-carousel"
-            data-slide-to="<?php echo $indic_idx?>"
-            <?php echo ($indic_idx == 0 ? 'class="active"' : '') ?>>
-        </li>
-      <?php $indic_idx++; ?>
-      <? endforeach; ?>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <?php $item_idx = 0; ?>
-      <?php foreach ($this->lead_items as &$item) : ?>
-        <?php
-          $this->item = &$item;
-          $this->idx = &$item_idx;
-          echo $this->loadTemplate('carousel_delegate');
-          $item_idx++;
-        ?>
-      <?php endforeach; ?>
-      <a class="left carousel-control"
-         href="#main-carousel"
-         role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"
-              aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control"
-         href="#main-carousel"
-         role="button"
-         data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"
-              aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-  </div>
-<?php endif; ?>
-<div class="container featurette-content">
-  <div class="row featurette">
-    <div class="col-md-12 text-center">
-      <h1 class="featurette-heading"><?php echo $this->escape($cfg->get('sitename')); ?></h1>
-      <p><span class="glyphicon glyphicon-menu-down"></span></p>
-      <hr>
-  </div>
+<div class="row featurette">
+  <div class="col-md-12 text-center">
+    <h1 class="featurette-heading"><?php echo $this->escape($cfg->get('sitename')); ?></h1>
+    <p><span class="glyphicon glyphicon-menu-down"></span></p>
+    <hr>
 </div>
 <?php ?>
