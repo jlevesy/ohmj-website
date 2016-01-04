cf5d0e988f1928633af32045ce47f2eb938c19d7
Julien Levesy <jlevesy@gmail.com>
2016-01-04 08:26:20 +0100

    setup readmore override

--- src/php/html/com_content/category/blog_item.php
+++ src/php/html/com_content/category/blog_item.php
@@ -1,5 +1,6 @@
 <?php
 defined('_JEXEC') or die;
+$params = $this->item->params;
 ?>
 <div class="row featurette">
   <div class="col-xs-12">
@@ -11,5 +12,18 @@ defined('_JEXEC') or die;
       <?php echo $this->escape($this->item->title); ?>
     </h2>
     <?php echo $this->item->introtext; ?>
+
+    <?php
+      if ($this->item->readmore) :
+        $link = JRoute::_(ContentHelperRoute::getArticleRoute(
+          $this->item->slug, $this->item->catid, $this->item->language
+        ));
+        echo JLayoutHelper::render('joomla.content.readmore', array(
+          'item'=> $this->item,
+          'params' => $params,
+          'link' => $link
+        ));
+      endif;
+    ?>
   </div>
 </div>
