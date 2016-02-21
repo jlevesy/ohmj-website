9dfbbed3097960956ff3c739696e54fc557a0386
Julien Levesy <jlevesy@gmail.com>
2016-02-21 16:01:13 +0100

    make the caroussel backgound image full width

--- src/php/modules/mod_ohmjcarousel/tmpl/default_item.php
+++ src/php/modules/mod_ohmjcarousel/tmpl/default_item.php
@@ -2,8 +2,9 @@
 defined('_JEXEC') or die;
 $images = json_decode($item->images);
 ?>
-<div class="item <?php echo ($item_idx == 0 ? 'active' : ''); ?>">
-  <div class="container" style="background-image: url(<?php echo $images->image_intro;?>);">
+<div class="item <?php echo ($item_idx == 0 ? 'active' : ''); ?>"
+     style="background-image: url(<?php echo $images->image_intro;?>);">
+  <div class="container">
     <div class="carousel-panel carousel-panel-top carousel-panel-<?php echo (($item_idx % 2) == 0 ? "left" : "right") ?>">
       <h1>
         <a href="<?php echo '/index.php?option=com_content&view=article&id='.$item->id?>">
