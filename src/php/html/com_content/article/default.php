dc73bad58ca96d58a23368cd3f8743b3e99c69b4
Julien Levesy <jlevesy@gmail.com>
2016-07-14 17:16:27 +0200

    style up things

--- src/php/html/com_content/article/default.php
+++ src/php/html/com_content/article/default.php
@@ -2,8 +2,19 @@
 defined('_JEXEC') or die;
 ?>
 <div class="article">
-  <h2 class="text-center">
+  <h2>
     <?php echo $this->escape($this->item->title); ?>
   </h2>
+  <div class="meta">
+    <p>
+      <?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON',
+                 JHtml::_('date', $this->item->pulish_up, JText::_('DATE_FORMAT_LC1'))); ?>
+    </p>
+    <p>
+      <?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $this->item->author); ?>
+    </p>
+  </div>
+  <?php echo $this->item->event->beforeDisplayContent; ?>
   <?php echo $this->item->text; ?>
+  <?php echo $this->item->event->afterDisplayContent; ?>
 </div>
